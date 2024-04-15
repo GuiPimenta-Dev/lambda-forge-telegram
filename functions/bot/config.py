eb79d3738bf5480a533a2cc46da4343c144ef22a
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-04-15 18:54:37 -0300

    jhony

--- functions/bot/config.py
+++ functions/bot/config.py
@@ -15,35 +15,35 @@ class BotConfig:
 
         services.api_gateway.create_endpoint("POST", "/bot", bot_function, public=True)
 
-        start_function = services.aws_lambda.create_function(
-            name="StartCommand",
-            path="./functions/start",
-            description="Handle /start command",
-        )
+        # start_function = services.aws_lambda.create_function(
+        #     name="StartCommand",
+        #     path="./functions/start",
+        #     description="Handle /start command",
+        # )
 
-        test_function = services.aws_lambda.create_function(
-            name="TestCommand",
-            path="./functions/test",
-            description="Handle /test command",
-        )
+        # test_function = services.aws_lambda.create_function(
+        #     name="TestCommand",
+        #     path="./functions/test",
+        #     description="Handle /test command",
+        # )
         
-        stm = services.state_machine
+        # stm = services.state_machine
 
-        # Create tasks (Lambda functions already defined)
-        task1 = stm.create_task("HandleStart", start_function)
-        task2 = stm.create_task("HandleTest", test_function)
+        # # Create tasks (Lambda functions already defined)
+        # task1 = stm.create_task("HandleStart", start_function)
+        # task2 = stm.create_task("HandleTest", test_function)
 
-        # Define choice state
-        choice = stm.add_choice(
-            "CommandChoice", [
-                (stm.string_equals("$.command", "/start"), task1),
-                (stm.string_equals("$.command", "/test"), task2)
-            ],
-        )
+        # # Define choice state
+        # choice = stm.add_choice(
+        #     "CommandChoice", [
+        #         (stm.string_equals("$.command", "/start"), task1),
+        #         (stm.string_equals("$.command", "/test"), task2)
+        #     ],
+        # )
         
-        # Finalize and build the state machine
-        state_machine = stm.finalize_and_build("TelegramBotStateMachine", choice)
+        # # Finalize and build the state machine
+        # state_machine = stm.finalize_and_build("TelegramBotStateMachine", choice)
 
-        state_machine.grant_start_execution(bot_function)
+        # state_machine.grant_start_execution(bot_function)
         
-        bot_function.add_environment("STATE_MACHINE_ARN", state_machine.state_machine_arn)
+        # bot_function.add_environment("STATE_MACHINE_ARN", state_machine.state_machine_arn)
--- functions/bot/main.py
+++ functions/bot/main.py
@@ -7,15 +7,16 @@ import os
 def lambda_handler(event, context):
     chat_id, text = telegram.get_chat_id(event), telegram.get_text(event)
     
-    sfn_client = boto3.client('stepfunctions')
+    telegram.send_message(chat_id, f"Received command: {text}")
+    # sfn_client = boto3.client('stepfunctions')
 
-    # Start the state machine execution
-    sfn_client.start_execution(
-        stateMachineArn=os.environ['STATE_MACHINE_ARN'],  
-        input=json.dumps({
-            "chat_id": chat_id,
-            "command": text
-        })
-    )
+    # # Start the state machine execution
+    # sfn_client.start_execution(
+    #     stateMachineArn=os.environ['STATE_MACHINE_ARN'],  
+    #     input=json.dumps({
+    #         "chat_id": chat_id,
+    #         "command": text
+    #     })
+    # )
     return {"statusCode": 200, "chat_id": 6180995571, "text": text, "command": text}
 
