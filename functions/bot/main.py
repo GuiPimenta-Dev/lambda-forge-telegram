53edcf976e905e0f5c1c5bd5f32cd94a56da88f8
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-04-15 17:47:09 -0300

    stm

--- functions/bot/main.py
+++ functions/bot/main.py
@@ -1,6 +1,20 @@
+import json
 import telegram
+import boto3
+import os
 
 
 def lambda_handler(event, context):
     chat_id, text = telegram.get_chat_id(event), telegram.get_text(event)
+    
+    sfn_client = boto3.client('stepfunctions')
+
+    # Start the state machine execution
+    sfn_client.start_execution(
+        stateMachineArn=os.environ['STATE_MACHINE_ARN'],  
+        input=json.dumps({
+            "chat_id": chat_id,
+            "command": text
+        })
+    )
     return {"statusCode": 200, "chat_id": chat_id, "text": text, "command": text}
