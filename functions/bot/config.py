d6c21d4ca3dcec496064878271025e815a753d50
Guilherme Alves pimenta <guialvespimenta27@gmail.com>
2024-04-15 17:19:57 -0300

    stm

--- functions/bot/config.py
+++ functions/bot/config.py
@@ -7,13 +7,13 @@ from aws_cdk import aws_stepfunctions_tasks as task
 class BotConfig:
     def __init__(self, services: Services) -> None:
 
-        function = services.aws_lambda.create_function(
+        bot_function = services.aws_lambda.create_function(
             name="Bot",
             path="./functions/bot",
             description="Telegram webhook",
         )
 
-        services.api_gateway.create_endpoint("POST", "/bot", function, public=True)
+        services.api_gateway.create_endpoint("POST", "/bot", bot_function, public=True)
 
         start_function = services.aws_lambda.create_function(
             name="StartCommand",
@@ -47,3 +47,7 @@ class BotConfig:
 
         # Finalize and build the state machine
         state_machine = stm.finalize_and_build("MyStateMachine", choice)
+
+        state_machine.grant_start_execution(bot_function)
+        
+        bot_function.add_environment("STATE_MACHINE_ARN", state_machine.state_machine_arn)
